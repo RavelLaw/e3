130fde4c4feb048bf15a0383a9eb3b0df11d7c11
Spencer <spencer@ravellaw.com>
2015-08-03 17:27:34 -0700

    No to two.js

--- blueprints/e3/index.js
+++ blueprints/e3/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('two');
+
   }
 };
