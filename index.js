12666b9c2624afbc346bda1296c1fb5459f391a9
Spencer <spencer@ravellaw.com>
2015-08-05 15:38:32 -0700

    Remove Two.js Dependency

--- index.js
+++ index.js
@@ -5,10 +5,5 @@ module.exports = {
   name: 'e3',
   included: function(app) {
     this._super.included(app);
-
-    app.import({
-      development: app.bowerDirectory + '/two.js/build/two.js',
-      production: app.bowerDirectory + '/two.js/build/two.min.js'
-    });
   }
 };
