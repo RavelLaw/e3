28cd2e42ed7461c4713148b4f32b96f62d31abab
Spencer <spencer@ravellaw.com>
2015-08-06 11:14:39 -0700

    Make Attribute Map a white-list

--- addon/utils/shadow/types/svg-attribute-map.js
+++ addon/utils/shadow/types/svg-attribute-map.js
@@ -1,7 +1,41 @@
+/*
+ This acts as both a way to normalize attribute names, but also acts
+ as a white list for the supported properties.
+ */
 export default {
   'circle': {
     'radius': 'r',
     'x': 'cx',
     'y': 'cy'
+  },
+  'group': {
+    'transform': 'transform'
+  },
+  'rect': {
+    'x': 'x',
+    'y': 'y',
+    'height': 'height',
+    'width': 'width',
+    'fill': 'fill',
+    'stroke': 'stroke',
+    'stroke-width': 'stroke-width'
+  },
+  'line': {
+    'x1': 'x1',
+    'x2': 'x2',
+    'y1': 'y1',
+    'y2': 'y2',
+    'stroke': 'stroke',
+    'stroke-width': 'stroke-width'
+  },
+  'path': {
+    'd': 'd',
+    'stroke': 'stroke',
+    'stroke-width': 'stroke-width',
+    'fill': 'fill'
+  },
+  'text': {
+    'x': 'x',
+    'y': 'y'
   }
 };
