25b45754faa7487b81b01ee535a346e047d0018d
Spencer <spencer@ravellaw.com>
2015-08-07 10:14:59 -0700

    Basic Tests for Render Types

--- tests/unit/utils/shadow/types/canvas-test.js
+++ tests/unit/utils/shadow/types/canvas-test.js
@@ -1,10 +1,23 @@
-import shadowTypesCanvas from '../../../utils/shadow/types/canvas';
+import shadowTypesCanvas from 'e3/utils/shadow/types/canvas';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | shadow/types/canvas');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  var result = shadowTypesCanvas();
-  assert.ok(result);
+test('implements all the necessary methods', function(assert) {
+  // var result = shadowTypesCanvas();
+  assert.ok(shadowTypesCanvas.group && typeof shadowTypesCanvas.group === 'function', 'has group function');
+  assert.ok(shadowTypesCanvas.circle && typeof shadowTypesCanvas.circle === 'function', 'has circle function');
+  assert.ok(shadowTypesCanvas.rectangle && typeof shadowTypesCanvas.rectangle === 'function', 'has rectangle function');
+  assert.ok(shadowTypesCanvas.line && typeof shadowTypesCanvas.line === 'function', 'has line function');
+  assert.ok(shadowTypesCanvas.path && typeof shadowTypesCanvas.path === 'function', 'has path function');
+  assert.ok(shadowTypesCanvas.text && typeof shadowTypesCanvas.text === 'function', 'has text function');
+  assert.ok(shadowTypesCanvas.stage && typeof shadowTypesCanvas.stage === 'function', 'has stage function');
+  assert.ok(shadowTypesCanvas.checkEvent && typeof shadowTypesCanvas.checkEvent === 'function', 'has checkEvent function');
+  assert.ok(!shadowTypesCanvas.destroy, 'does not have a destroy function as it is implicitly destroyed by not being rendered anymore');
 });
+
+/*
+ TODO: Create an object that implements the same interface as a convas context to test that the correct
+ hooks are utilized for each of the shape types. Make tests against this.
+ */
--- tests/unit/utils/shadow/types/svg-test.js
+++ tests/unit/utils/shadow/types/svg-test.js
@@ -1,10 +1,23 @@
-import shadowTypesSvg from '../../../utils/shadow/types/svg';
+import shadowTypesSvg from 'e3/utils/shadow/types/svg';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | shadow/types/svg');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  var result = shadowTypesSvg();
-  assert.ok(result);
+test('implements all the necessary methods', function(assert) {
+  // var result = shadowTypesSvg();
+  assert.ok(shadowTypesSvg.group && typeof shadowTypesSvg.group === 'function', 'has group function');
+  assert.ok(shadowTypesSvg.circle && typeof shadowTypesSvg.circle === 'function', 'has circle function');
+  assert.ok(shadowTypesSvg.rectangle && typeof shadowTypesSvg.rectangle === 'function', 'has rectangle function');
+  assert.ok(shadowTypesSvg.line && typeof shadowTypesSvg.line === 'function', 'has line function');
+  assert.ok(shadowTypesSvg.path && typeof shadowTypesSvg.path === 'function', 'has path function');
+  assert.ok(shadowTypesSvg.text && typeof shadowTypesSvg.text === 'function', 'has text function');
+  assert.ok(shadowTypesSvg.stage && typeof shadowTypesSvg.stage === 'function', 'has stage function');
+  assert.ok(shadowTypesSvg.checkEvent && typeof shadowTypesSvg.checkEvent === 'function', 'has checkEvent function');
+  assert.ok(shadowTypesSvg.destroy && typeof shadowTypesSvg.destroy === 'function', 'has destroy function');
 });
+
+/*
+ TODO: Create an object that implements the same interface as an svg/group element to test that the correct
+ hooks are utilized for each of the shape types. Make tests against this.
+ */
