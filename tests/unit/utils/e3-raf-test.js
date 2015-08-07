368ec21258cebcafb361aad0120c031578982b18
Spencer <spencer@ravellaw.com>
2015-08-07 09:43:46 -0700

    Add Simple RAF Test

--- tests/unit/utils/e3-raf-test.js
+++ tests/unit/utils/e3-raf-test.js
@@ -1,10 +1,18 @@
 import e3Raf from '../../../utils/e3-raf';
+import Ember from 'ember';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | e3 raf');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var result = e3Raf();
-  assert.ok(result);
+  let RAF = e3Raf();
+  let done = assert.async();
+
+  Ember.run(() => {
+    RAF(time => {
+      assert.ok(time, 'request animation frame works.');
+      done();
+    });
+  });
 });
