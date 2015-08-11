520df649fd4d6b05ef3465dfcf5d352b3947cedf
Spencer <spencer@ravellaw.com>
2015-08-11 13:14:29 -0700

    support clamp / round on linear scales

--- addon/utils/shadow/scales/linear.js
+++ addon/utils/shadow/scales/linear.js
@@ -1,9 +1,10 @@
+const {min, max, round:mathRound} = Math;
 /*
  Accepts a range, domain and a hash of options and returns a function that converts
  a provided value into the output range.
  */
-export default function shadowScalesLinear(range, domain, options) {
-  let {clamp} = options;
+export default function shadowScalesLinear(range = [0,1], domain = [0,1], options = {}) {
+  let {clamp, round} = options;
   let [r0, r1] = range;
   let [d0, d1] = domain;
   let rDelta = r1 - r0;
@@ -11,9 +12,24 @@ export default function shadowScalesLinear(range, domain, options) {
 
   return function(val) {
     if(dDelta === 0 || rDelta === 0) {
-      return 0;
+      // Divide by 0; set as the range's origin
+      return r0;
     } else {
-      return (val - d0) / dDelta * rDelta + r0;
+      // Calculate the result linearly.
+      let result = (val - d0) / dDelta * rDelta + r0;
+
+      // If we should clamp the result, do it.
+      if(clamp) {
+        result = min(result, r1);
+        result = max(r0, result);
+      }
+
+      // If we should round the number
+      if(round) {
+        result = mathRound(result);
+      }
+
+      return result;
     }
   };
 }
--- tests/unit/utils/shadow/scales/linear-test.js
+++ tests/unit/utils/shadow/scales/linear-test.js
@@ -1,10 +1,64 @@
-import shadowScalesLinear from '../../../utils/shadow/scales/linear';
+import shadowScalesLinear from 'e3/utils/shadow/scales/linear';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | shadow/scales/linear');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var result = shadowScalesLinear();
+  // The default is to return an identity scale.
+  let result = shadowScalesLinear();
   assert.ok(result);
+  assert.equal(result(0.5), 0.5);
+  assert.equal(result(0), 0);
+  assert.equal(result(1), 1);
+  assert.equal(result(-1), -1);
+});
+
+test('do basic linear inerpolation tests', function(assert) {
+  let result = shadowScalesLinear([0,100], [0,10]);
+  assert.equal(result(5), 50);
+  assert.equal(result(-5), -50);
+  assert.equal(result(10), 100);
+  assert.equal(result(15), 150);
+});
+
+test('do basic linear inerpolation tests without 0 base', function(assert) {
+  let result = shadowScalesLinear([90,100], [0,10]);
+  assert.equal(result(5), 95);
+  assert.equal(result(-5), 85);
+  assert.equal(result(10), 100);
+  assert.equal(result(15), 105);
+});
+
+test('do basic linear inerpolation tests with clamping', function(assert) {
+  let result = shadowScalesLinear([0,100], [0,10], {
+    clamp: true
+  });
+
+  assert.equal(result(5), 50);
+  assert.equal(result(-5), 0);
+  assert.equal(result(10), 100);
+  assert.equal(result(15), 100);
+});
+
+test('do basic linear inerpolation tests with clamping non-zero base', function(assert) {
+  let result = shadowScalesLinear([90,100], [0,10], {
+    clamp: true
+  });
+
+  assert.equal(result(5), 95);
+  assert.equal(result(-5), 90);
+  assert.equal(result(10), 100);
+  assert.equal(result(15), 100);
+});
+
+test('make the round option return only integers', function(assert) {
+  let result = shadowScalesLinear([5,10], [0,10]);
+
+  let resultRound = shadowScalesLinear([5,10], [0,10], {
+    round: true
+  });
+
+  assert.equal(result(5), 7.5);
+  assert.equal(resultRound(5), 8);
 });
