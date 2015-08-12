fea57d984726f7bbe64ca7454bdad38ec62592da
Spencer <spencer@ravellaw.com>
2015-08-11 17:38:23 -0700

    Support Scale Different Option on Bind-scale

--- addon/helpers/e3-bind-scale.js
+++ addon/helpers/e3-bind-scale.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
-const {isArray} = Ember;
+const {isArray, get} = Ember;
 const {keys} = Object;
 
-export function e3BindScale(params) {
+export function e3BindScale(params, options) {
+  let {scaleDifference} = options;
   let [scale, key] = params;
   if(scale) {
     var resultScale = function(data) {
       if(isArray(data)) {
         return data.map(item => {
-          return scale(item, key);
+          return calculateValue(get(item, key), scale, scaleDifference);
         });
       } else {
-        return scale(data, key);
+        return calculateValue(get(data, key), scale, scaleDifference);
       }
     };
 
@@ -22,6 +23,20 @@ export function e3BindScale(params) {
 
     return resultScale;
   }
+}
+
+function calculateValue(input, scale, scaleDifference) {
+  let result = scale(input);
+
+  /*
+   Inverting the output would take the
+   */
+  if(scaleDifference) {
+    let [r0, r1] = scale.range;
+    result = r1 - r0 - result;
+  }
+
+  return result;
 }
 
 export default Ember.Helper.helper(e3BindScale);
