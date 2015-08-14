b8228b00a44345e57438dcf0721a9f8d16bae265
Spencer <spencer@ravellaw.com>
2015-08-13 20:03:17 -0700

    Children Might be ready before groups

--- addon/components/e3-group.js
+++ addon/components/e3-group.js
@@ -3,45 +3,51 @@ import layout from '../templates/components/e3-container';
 import Group from '../utils/shadow/group';
 import e3AnimatedChild from '../mixins/e3-animated-child';
 import meta from '../mixins/e3-meta';
-const {get, set} = Ember;
+const {get, set, computed} = Ember;
 
 export default Ember.Component.extend(e3AnimatedChild, meta, {
   shadowType: 'group',
   layout: layout,
-
-  enterState: {
-    x: 0
-  },
-
-  activeState: {
-    x: 300
-  },
-
-  /*
-   We want to be able to yield the transform of this group.
-   */
-  matrix: null,
+  _shadowRegistrationQueue: computed(function() {
+    return Ember.A();
+  }),
 
   /*
    Generate the shadow object.
    */
   generateShadowObject(contextType, attrs) {
     let shadow = new Group(this, 'group', contextType, attrs);
+    let queue = get(this, '_shadowRegistrationQueue');
+    set(this, '_shadowRegistrationQueue', []);
+
     set(this, 'shadow', shadow);
+    queue.forEach(childShadow => {
+      shadow.add(childShadow);
+    });
   },
 
   /*
    Since this is both a context and a child, override the behaviors.
    */
   register(child) {
-    get(this, 'shadow').add(get(child, 'shadow'));
+    let shadow = get(this, 'shadow');
+    if(shadow) {
+      get(this, 'shadow').add(get(child, 'shadow'));
+    } else {
+      get(this, '_shadowRegistrationQueue').pushObject(get(child, 'shadow'));
+    }
   },
 
   unregister(child) {
-    get(this, 'shadow').remove(get(child, 'shadow'));
+    let shadow = get(this, 'shadow');
+    if(shadow) {
+      get(this, 'shadow').remove(get(child, 'shadow'));
+    } else {
+      get(this, '_shadowRegistrationQueue').removeObject(get(child, 'shadow'));
+    }
   },
 
   addToQueue(callback) {
-    return this.getAttr('context').addToQueue(callback);
+    return this.getAttr('_e3Context').addToQueue(callback);
   }
 });
