e3763c98af58e4961bc7aa788bcb72934c0d35ac
Ben Holmes <mail@benholmes.uk>
2015-08-17 00:06:27 -0700

    Create separate classes for path commands
    Since two path commands may have the same number of arguments

--- tests/unit/utils/shadow/line-interpolation/monotone-test.js
+++ tests/unit/utils/shadow/line-interpolation/monotone-test.js
@@ -1,4 +1,5 @@
 import monotone from 'ember-e3/utils/shadow/line-interpolation/monotone';
+import { Point, SmoothCurve, BezierCurve } from 'ember-e3/utils/shadow/types/commands';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | shadow/line interpolation/monotone');
@@ -7,12 +8,13 @@ test('it has a wide repetoire of commands', function(assert) {
   var points = [[0, 0], [1, 1], [2, 1], [3, 0], [4, 0]];
 
   // a + (a - b) = 2a - b where a is the position and b is the last control point
-  assert.deepEqual(monotone(points), [
-    [0, 0],
-    [0.08333333333333333, 0.08333333333333333, 0.6666666666666667, 1, 1, 1],
-    [1.3333333333333333, 1, 1.6666666666666667, 1, 2, 1],
-    [2.3333333333333333, 1, 2.6666666666666665, 0, 3, 0],
-    [3.3333333333333335, 0, 3.8333333333333335, 0, 4, 0]
+  var commands = monotone(points);
+  assert.deepEqual(commands, [
+    new Point(0, 0),
+    new BezierCurve(0.08333333333333333, 0.08333333333333333, 0.6666666666666667, 1, 1, 1),
+    new SmoothCurve(1.3333333333333333, 1, 1.6666666666666667, 1, 2, 1),
+    new SmoothCurve(2.3333333333333333, 1, 2.6666666666666665, 0, 3, 0),
+    new SmoothCurve(3.3333333333333335, 0, 3.8333333333333335, 0, 4, 0)
   ]);
 });
 
