bd1ab3833af076a467bd4919d6a8431d0cf5dfb5
Spencer <spencer@ravellaw.com>
2015-08-27 17:38:43 -0700

    rm console.log

--- addon/helpers/e3-ticks.js
+++ addon/helpers/e3-ticks.js
@@ -4,7 +4,6 @@ import utilE3Ticks from '../utils/e3-ticks';
 export function e3Ticks(params, hash) {
   let ticks = hash.ticks || 10;
   let [domain] = params;
-  console.log(domain, ticks);
   return utilE3Ticks(domain, ticks);
 }
 
