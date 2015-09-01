8a273ee799eddd3989073bc8b496fa71ef2fa375
Spencer <spencer@ravellaw.com>
2015-08-31 17:47:02 -0700

    Reset Defaults for Text/Line

--- addon/components/e3-shape/text.js
+++ addon/components/e3-shape/text.js
@@ -3,11 +3,7 @@ import e3AnimatedChild from '../e3-animated-child';
 export default e3AnimatedChild.extend({
   shadowType: 'text',
 
-  enterState: {
-    x: 0,
-    y: 0,
-    text: ''
-  },
+  enterState: {},
 
   activeState: {
     x: null,
