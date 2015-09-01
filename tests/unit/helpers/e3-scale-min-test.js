d991efec8f269c3b56c4b941d75bdd0f2f45893a
Spencer <spencer@ravellaw.com>
2015-08-31 17:47:26 -0700

    Add Helpers for Scale Min/max

---
+++ tests/unit/helpers/e3-scale-min-test.js
@@ -0,0 +1,10 @@
+import { e3ScaleMin } from '../../../helpers/e3-scale-min';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | e3 scale min');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = e3ScaleMin(42);
+  assert.ok(result);
+});
