b2f286b2454ad12df3e820780edd1916116ffe48
Spencer <spencer@ravellaw.com>
2015-09-04 17:24:12 -0700

    Add e3-bind-layout

---
+++ tests/unit/helpers/e3-bind-layout-test.js
@@ -0,0 +1,10 @@
+import { e3BindLayout } from '../../../helpers/e3-bind-layout';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | e3 bind layout');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = e3BindLayout(42);
+  assert.ok(result);
+});
