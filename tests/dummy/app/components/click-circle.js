086248e3c6cd1090bd0ec3807c0f8274d299a02d
Spencer <spencer@ravellaw.com>
2015-09-21 11:32:38 -0700

    jshint.

--- tests/dummy/app/components/click-circle.js
+++ tests/dummy/app/components/click-circle.js
@@ -2,6 +2,7 @@ import circle from 'ember-e3/components/e3-shape/circle';
 
 export default circle.extend({
   click(event, data) {
+    /* global alert */
     alert('Clicked: '+JSON.stringify(data));
   },
   mouseMove(event, data) {
